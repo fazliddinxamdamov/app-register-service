71b5df7cf1daff681098745cce07279b3fc96093
fazliddin <fazliddinxamdamov102@gmail.com>
2022-04-06 16:34:57 +0500

    Bismillah

---
+++ src/test/java/com/fazliddin/appregisterservice/AppRegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazliddin.appregisterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRegisterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
